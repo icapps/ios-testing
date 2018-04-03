513baccbf404268b676b3b8d1cf176baeaea47ab
Herre <ikbenhansvanherreweghe@gmail.com>
2018-04-03 09:04:09 +0200

    add some methods

--- Testing/Modules/ViewModel.swift
+++ Testing/Modules/ViewModel.swift
@@ -8,7 +8,31 @@
 
 import Foundation
 
+protocol GreetingGenerating {
+    func greeting(with username:String?) -> String
+}
+
+class GreetingGenerator:GreetingGenerating {
+    func greeting(with username:String?) -> String {
+        if let username = username {
+            return "Hello \(username)!"
+        } else {
+            return "Hello!"
+        }
+    }
+}
+
+protocol ViewModelDelegate:class {
+    func didChangeSomething(_ sender:ViewModel)
+}
+
 class ViewModel {
+    
+    typealias CompletionHandler = (()->())
+    
+    private let greetingGenerator:GreetingGenerating
+    
+    weak var delegate:ViewModelDelegate?
     
     // MARK: - Username
     
@@ -16,11 +40,27 @@ class ViewModel {
     
     // MARK: - Labels
     
-    var titleLabel: String {
-        if let username = username {
-            return "Hello \(username)!"
-        } else {
-            return "Hello!"
-        }
+    let titleLabebl: String = "Hello world"
+    let firstButtonText: String = "Do something async"
+    let secondButtonText: String = "Do something else"
+    
+    var greetingLabel: String {
+        return greetingGenerator.greeting(with: username)
+    }
+    
+    // MARK: - Initialization
+    
+    init(greetingGenerator:GreetingGenerating = GreetingGenerator()) {
+        self.greetingGenerator = greetingGenerator
+    }
+    
+    // MARK: Public
+    
+    func doSomethingAsync(completion:CompletionHandler) {
+        completion()
+    }
+    
+    func changeSomething() {
+        self.delegate?.didChangeSomething(self)
     }
 }
